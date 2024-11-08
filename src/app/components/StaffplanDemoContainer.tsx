add62bfa56a98c5f45fbb82f6e0b04ff558b3f41
Oleksandr Zhukov <sashazh316@gmail.com>
2024-11-08 20:53:15 +0200

    fixed image sizes

--- src/app/components/StaffplanDemoContainer.tsx
+++ src/app/components/StaffplanDemoContainer.tsx
@@ -10,13 +10,13 @@ const StaffplanDemoContainer: React.FC<StaffplanDemoContainerProps> = ({ childre
         <section className="min-h-screen flex flex-col items-center justify-between bg-demo-gradient pb-6">
             <h1 className="text-huge font-bold mb-4">Try Staffplan!</h1>
             {children}
-            <div className="relative w-full max-w-[95vw] h-[95vh] aspect-[16/9] mb-8 px-4">
+            <div className="relative w-full max-w-[95vw] h-[auto] aspect-[16/9] mb-8 px-4">
                 <Image
                     src="/image.png"
                     alt="Description of image"
                     fill
-                    sizes="100vw"
-                    className="rounded-lg"
+                    sizes="(max-width: 640px) 80vw, (max-width: 768px) 80vw, (max-width: 1024px) 70vw, 50vw"
+                    className="rounded-lg object-cover"
                 />
             </div>
             <RoundedButton text="Sign me up!" variant="filled" size="large" href="https://staffplan.com/sign_in" />
