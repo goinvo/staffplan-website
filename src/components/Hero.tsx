e70e8ee2c676038fa2b7b48e6a8eebd3a45da26c
Eric Benoit <benwa02@gmail.com>
2025-03-19 14:00:53 -0400

    Updating hero width.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ import CallToActionButton from './CallToActionButton';
 const Hero: React.FC = () => {
   return (
     <div className="flex flex-col">
-      <div className="container mx-auto px-4 pt-20 pb-32 flex flex-col items-start justify-center relative">
+      <div className="container mx-auto px-4 max-w-5xl pt-20 pb-32 flex flex-col items-start justify-center relative">
         <h1 
           className="text-5xl md:text-7xl lg:text-9xl font-bold tracking-tight mb-10 animate-float-up text-left text-teal"
           style={{ animationDelay: '0.4s' }}
